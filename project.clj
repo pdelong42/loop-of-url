2e5a2b880ced770111c5ee6d842276487e9883ea
Paul DeLong <Paul.DeLong@gmail.com>
2016-12-25 12:14:09 -0500

    [dev] update to Clojure 1.6.0
    The project still builds and runs after updating Clojure to 1.6.0.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
             javax.jms/jms
             com.sun.jmdk/jmxtools
             com.sun.jmx/jmxri  ]  ]
-      [org.clojure/clojure       "1.5.1"]
+      [org.clojure/clojure       "1.6.0"]
       [org.clojure/tools.cli     "0.3.1"]
       [org.clojure/tools.logging "0.2.6"]
       [org.slf4j/slf4j-log4j12   "1.7.1"]  ]
