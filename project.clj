ea65df77f067800ca365e2a6e02706d7914a5df5
Paul DeLong <Paul.DeLong@gmail.com>
2016-12-27 13:30:23 -0500

    [dev] updated tools.cli to latest minor release;

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
             com.sun.jmdk/jmxtools
             com.sun.jmx/jmxri  ]  ]
       [org.clojure/clojure       "1.8.0"]
-      [org.clojure/tools.cli     "0.3.1"]
+      [org.clojure/tools.cli     "0.3.5"]
       [org.clojure/tools.logging "0.2.6"]
       [org.slf4j/slf4j-log4j12   "1.7.1"]  ]
    :main ^:skip-aot loop-of-url.core
