8b891bb7077761db5fb534bab72791b95d17cf88
Paul DeLong <Paul.DeLong@gmail.com>
2017-01-11 09:22:10 -0500

    [dev] some tests I'm trying to get working
    I can't seem to figure-out a good way to gets for the event of DNS
    resolution failure.  These are commented-out for now, until I can get
    them working.

--- test/loop_of_url/core_test.clj
+++ test/loop_of_url/core_test.clj
@@ -5,12 +5,11 @@
       [loop-of-url.core :refer :all]  )  )
 ;      [stub-http.core   :refer :all]
 
-
 (deftest basic-http-get
    (testing "basic exercise of the http-get function"
       (is
          (= {  :status 200 :body "Testing 1...2...3"  }
-            (with-global-fake-routes-in-isolation
+            (with-global-fake-routes
                {  "http://example.com/foo"
                   (fn
                      [request]
@@ -20,3 +19,27 @@
                   [  [status body remaining message]
                      (http-get "http://example.com/foo" 1)  ]
                   {  :status status :body body  }  )  )  )  )  )  )
+
+;(deftest http-get-of-nxdomain
+;   (testing "validate whether http-get handles java.net.UnknownHostException"
+;      (is
+;         (= {  :status "DNS resolution failure" :body ""  }
+;            (with-global-fake-routes
+;               {  "http://example.com/foo"
+;                  (fn
+;                     [request]
+;                     {  :status 200
+;                        :body "Testing 1...2...3"  }  )  }
+;               (let
+;                  [  [status body remaining message]
+;                     (http-get "http://foo.paul.delong.name/" 1)  ]
+;                  {  :status status :body body  }  )  )  )  )  )  )
+
+;(deftest http-get-of-nxdomain
+;   (testing "validate whether http-get handles java.net.UnknownHostException"
+;      (is
+;         (= {  :status "DNS resolution failure" :body ""  }
+;            (let
+;               [  [status body remaining message]
+;                  (http-get "http://foo.paul.delong.name/" 1)  ]
+;               {  :status status :body body  }  )  )  )  )  )
